d7e925cead34e4a9f9c28cae513ce097f0743b3f
kaden-fugate <kkadenfugate@gmail.com>
2023-12-16 15:01:28 -0800

    Move Validation
    Working on being able to get all possible moves for all possible pieces on the board. Also removed the square class. Piece class absorbed all of its additional data members. Havent fully finished move validation for pawn as we will need to add en passant as well as make it so that it can only move to diagonally if there is a piece of opposite color present in the diagonal squares. Next objective to complete (after pawn move validation) is making a function to actually move the pieces. this will involve a functionality to capture. with moving a piece, we will also need to make sure that moving that piece wont expose the king to an attack.

--- classes/board.py
+++ classes/board.py
@@ -1,13 +1,19 @@
-from square import Square
 from pieces import King, Queen, Rook, Bishop, Knight, Pawn
 
 class Board:
 
     # board initialization
     def __init__(self):
-        self.board = [[Square(i, j, None) for i in range(0,8)] for j in range(0,8)]
+
+        # init mem for board, init white and black pieces
+        self.board = [[None for i in range(0,8)] for j in range(0,8)]
         self.initColor('white')
         self.initColor('black')
+
+        # set all valid moves for each piece on the board
+        for rank in (self.board[0], self.board[1], self.board[6], self.board[7]):
+            for piece in rank:
+                piece.setValidMoves(self.board)
     
     # init pieces for a given color
     def initColor(self, color):
@@ -16,20 +22,20 @@ class Board:
         rank = 7 if color == 'white' else 0
 
         # set the pieces on the back rank
-        self.board[rank][0].setPiece( Rook(color) )
-        self.board[rank][1].setPiece( Knight(color) )
-        self.board[rank][2].setPiece( Bishop(color) )
-        self.board[rank][3].setPiece( Queen(color) )
-        self.board[rank][4].setPiece( King(color) )
-        self.board[rank][5].setPiece( Bishop(color) )
-        self.board[rank][6].setPiece( Knight(color) )
-        self.board[rank][7].setPiece( Rook(color) )
+        self.board[rank][0] = Rook(0, rank, color)
+        self.board[rank][1] = Knight(1, rank, color)
+        self.board[rank][2] = Bishop(2, rank, color)
+        self.board[rank][3] = Queen(3, rank, color)
+        self.board[rank][4] = King(4, rank, color)
+        self.board[rank][5] = Bishop(5, rank, color)
+        self.board[rank][6] = Knight(6, rank, color)
+        self.board[rank][7] = Rook(7, rank, color)
 
         # get pawns rank
         rank = 6 if rank == 7 else 1
 
         # set the pawns 
-        for i in range(0,8): self.board[rank][i].setPiece(Pawn(color))
+        for i in range(0,8): self.board[rank][i] = Pawn(i, rank, color)
 
     # function to print board
     def printBoard(self):
@@ -40,10 +46,128 @@ class Board:
         # rows in board
         for row in self.board:
             print("|     |     |     |     |     |     |     |     |")
-            print("|", end=" ")
+            print("|", end="")
             for square in row:
-                square.printPiece()
+                if square:
+                    print(f"  {square.getIcon()}  |", end="")
+                else: print("     |", end="")
             print("\n|_____|_____|_____|_____|_____|_____|_____|_____|")
 
-board = Board()
-board.printBoard()
+    def notationToPos(self, notation):
+
+        pos_x, pos_y = -1, -1
+
+        # convert rank to y pos
+        if notation[0] == 'A': pos_y = 0
+        elif notation[0] == 'B': pos_y = 1
+        elif notation[0] == 'C': pos_y = 2
+        elif notation[0] == 'D': pos_y = 3
+        elif notation[0] == 'E': pos_y = 4
+        elif notation[0] == 'F': pos_y = 5
+        elif notation[0] == 'G': pos_y = 6
+        elif notation[0] == 'H': pos_y = 7
+
+        # convert file to x pos
+        if notation[1] == '1': pos_x = 0
+        elif notation[1] == '2': pos_x = 1
+        elif notation[1] == '3': pos_x = 2
+        elif notation[1] == '4': pos_x = 3
+        elif notation[1] == '5': pos_x = 4
+        elif notation[1] == '6': pos_x = 5
+        elif notation[1] == '7': pos_x = 6
+        elif notation[1] == '8': pos_x = 7
+
+        return (pos_x, pos_y)
+    
+    def possibleMoves(self, pos_x, pos_y):
+
+        color = self.board[pos_y][pos_x].getColor()
+
+        # possible valid squares in row and col
+        col = []
+        x, y = pos_x, pos_y + 1
+        while y < 8:
+            if self.board[y][x] and self.board[y][x].getColor() == color: break
+            col.append((x, y))
+            if self.board[y][x] and self.board[y][x].getColor() != color: break
+            y += 1
+        
+        y = pos_y - 1
+        while y >= 0:
+            if self.board[y][x] and self.board[y][x].getColor() == color: break
+            col.append((x, y))
+            if self.board[y][x] and self.board[y][x].getColor() != color: break
+            y -= 1
+        
+        row = []
+        x, y = pos_x + 1, pos_y
+        while x < 8:
+            if self.board[y][x] and self.board[y][x].getColor() == color: break
+            row.append((x, y))
+            if self.board[y][x] and self.board[y][x].getColor() != color: break
+            x += 1
+        
+        x = pos_x - 1
+        while x >= 0:
+            if self.board[y][x] and self.board[y][x].getColor() == color: break
+            row.append((x, y))
+            if self.board[y][x] and self.board[y][x].getColor() != color: break
+            x -= 1
+
+        # squares to left and up in the left-to-right diagonal
+        ltr_diagonal = []
+        x, y = pos_x-1, pos_y-1
+        while x >= 0 and y >= 0:
+            if self.board[y][x] and self.board[y][x].getColor() == color: break
+            ltr_diagonal.append((x, y))
+            if self.board[y][x] and self.board[y][x].getColor() != color: break
+            x -= 1
+            y -= 1
+
+        # squares to right and down in left to right diagonal
+        x, y = pos_x+1, pos_y+1
+        while x < 8 and y < 8:
+            if self.board[y][x] and self.board[y][x].getColor() == color: break
+            ltr_diagonal.append((x, y))
+            if self.board[y][x] and self.board[y][x].getColor() != color: break
+            x += 1
+            y += 1
+        
+        # squares to right and up in right to left diagonal
+        rtl_diagonal = []
+        x, y = pos_x-1, pos_y+1
+        while x >= 0 and y < 8:
+            if self.board[y][x] and self.board[y][x].getColor() == color: break
+            rtl_diagonal.append((x, y))
+            if self.board[y][x] and self.board[y][x].getColor() != color: break
+            x -= 1
+            y += 1
+
+        # squares to left and down in right to left diagonal
+        x, y = pos_x+1, pos_y-1
+        while x < 8 and y >= 0:
+            if self.board[y][x] and self.board[y][x].getColor() == color: break
+            rtl_diagonal.append((x, y))
+            if self.board[y][x] and self.board[y][x].getColor() != color: break
+            x += 1
+            y -= 1
+        
+        # squares in knight moves
+        knight_dirs = [(-1,2),(1,2),(2,1),(2,-1),(-2,1),(-2,-1),(-1,-2),(1,-2)]
+        knight_moves = []
+        for x, y in knight_dirs:
+            x += pos_x
+            y += pos_y
+            if 0 <= x < 8 and 0 <= y < 8:
+                if self.board[y][x] and self.board[y][x].getColor() == color: break
+                knight_moves.append((x, y))
+        
+        # tuple with found moves
+        return (col, row, ltr_diagonal, rtl_diagonal, knight_moves)
+
+chess = Board()
+chess.printBoard()
+for idx_row, row in enumerate(chess.board):
+    for idx_col, piece in enumerate(row):
+        if piece:
+            print(f"POSSIBLE MOVES FOR {piece.getIcon()} AT ({idx_col},{idx_row}): {piece.getValidMoves()}")
--- classes/pieces.py
+++ classes/pieces.py
@@ -3,60 +3,155 @@
 # as king or pawn.
 class Piece:
     
-    def __init__(self, color):
+    def __init__(self, pos_x, pos_y, color):
         self.color = color
-        self.valid_moves = None
+        self.validMoves = []
+        self.pos_x = pos_x
+        self.pos_y = pos_y
     
-    def printIcon():
+    def getColor(self):
+        return self.color
+
+    def getIcon():
         pass
 
-    def getValidMoves():
-        pass
+    def setValidMoves(self, board):
+        
+        for dx, dy in self.directions:
 
+            # check is within the board
+            if 0 <= self.pos_x + dx < 8 and 0 <= self.pos_y + dy < 8:
+
+                board_pos = board[self.pos_y + dy][self.pos_x + dx]
+                
+                # check move does not put king in check
+                if not board_pos or (board_pos and board_pos.getColor() != self.color):
+                    self.validMoves.append((self.pos_x + dx, self.pos_y + dy))
+    
+    def getValidMoves(self):
+        return self.validMoves
+
+# King class
 class King(Piece):
 
-    def __init__(self, color):
-        super().__init__(color)
+    # initialize king class
+    def __init__(self, pos_x, pos_y, color):
+        super().__init__(pos_x, pos_y, color)
         self.icon = '\u265A' if color == 'white' else '\u2654'
+        self.directions = [(-1,1), (0,1), (1,1),
+                           (-1,0),        (1,0),
+                           (-1,-1),(0,-1),(1,-1)]
 
+    # kings icon
     def getIcon(self): return self.icon
+                
+    def isCheck(self):
+        return False
 
 class Queen(Piece):
 
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, pos_x, pos_y, color):
+        super().__init__(pos_x, pos_y, color)
         self.icon = '\u265B' if color == 'white' else '\u2655'
+        self.directions = [[(-1,1),(-2,2),(-3,3),(-4,4),(-5,5),(-6,6),(-7,7)],
+                           [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7)],
+                           [(-1,-1),(-2,-2),(-3,-3),(-4,-4),(-5,-5),(-6,-6),(-7,-7)],
+                           [(1,-1),(2,-2),(3,-3),(4,-4),(5,-5),(6,-6),(7,-7)],
+                           [(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7)], 
+                           [(-1,0),(-2,0),(-3,0),(-4,0),(-5,0),(-6,0),(-7,0)],       
+                           [(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)],
+                           [(0,-1),(0,-2),(0,-3),(0,-4),(0,-5),(0,-6),(0,-7)]]
 
     def getIcon(self): return self.icon
+
+    def setValidMoves(self, board):
+        for direction in self.directions:
+
+            for dx, dy in direction:
+
+                # check is within the board
+                if 0 <= self.pos_x + dx < 8 and 0 <= self.pos_y + dy < 8:
+
+                    board_pos = board[self.pos_y + dy][self.pos_x + dx]
+                    
+                    # check move does not put king in check
+                    if not board_pos or (board_pos and board_pos.getColor() != self.color):
+                        self.validMoves.append((self.pos_x + dx, self.pos_y + dy))
+                    else: break
 
 class Rook(Piece):
 
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, pos_x, pos_y, color):
+        super().__init__(pos_x, pos_y, color)
         self.icon = '\u265C' if color == 'white' else '\u2656'
+        self.directions = [ [(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7)], 
+                            [(-1,0),(-2,0),(-3,0),(-4,0),(-5,0),(-6,0),(-7,0)],       
+                            [(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)],
+                            [(0,-1),(0,-2),(0,-3),(0,-4),(0,-5),(0,-6),(0,-7)]
+                            ]
 
     def getIcon(self): return self.icon
+
+    def setValidMoves(self, board):
+        for direction in self.directions:
+
+            for dx, dy in direction:
+
+                # check is within the board
+                if 0 <= self.pos_x + dx < 8 and 0 <= self.pos_y + dy < 8:
+
+                    board_pos = board[self.pos_y + dy][self.pos_x + dx]
+                    
+                    # check move does not put king in check
+                    if not board_pos or (board_pos and board_pos.getColor() != self.color):
+                        self.validMoves.append((self.pos_x + dx, self.pos_y + dy))
+                    else: break
 
 class Bishop(Piece):
 
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, pos_x, pos_y, color):
+        super().__init__(pos_x, pos_y, color)
         self.icon = '\u265D' if color == 'white' else '\u2657'
+        self.directions = [[(-1,1),(-2,2),(-3,3),(-4,4),(-5,5),(-6,6),(-7,7)],
+                           [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7)],
+                           [(-1,-1),(-2,-2),(-3,-3),(-4,-4),(-5,-5),(-6,-6),(-7,-7)],
+                           [(1,-1),(2,-2),(3,-3),(4,-4),(5,-5),(6,-6),(7,-7)]]
 
     def getIcon(self): return self.icon
 
+    def setValidMoves(self, board):
+        for direction in self.directions:
+
+            for dx, dy in direction:
+
+                # check is within the board
+                if 0 <= self.pos_x + dx < 8 and 0 <= self.pos_y + dy < 8:
+
+                    board_pos = board[self.pos_y + dy][self.pos_x + dx]
+                    
+                    # check move does not put king in check
+                    if not board_pos or (board_pos and board_pos.getColor() != self.color):
+                        self.validMoves.append((self.pos_x + dx, self.pos_y + dy))
+                    else: break
+
 class Knight(Piece):
 
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, pos_x, pos_y, color):
+        super().__init__(pos_x, pos_y, color)
         self.icon = '\u265E' if color == 'white' else '\u2658'
+        self.directions = [      (-1,2),  (1,2),
+                           (-2,1),              (2,1),
+                           (-2,-1),             (2,-1),
+                                 (-1,-2),  (1,-2)]
 
     def getIcon(self): return self.icon
 
 class Pawn(Piece):
 
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, pos_x, pos_y, color):
+        super().__init__(pos_x, pos_y, color)
         self.icon = '\u265F' if color == 'white' else '\u2659'
+        self.directions = [       (0,2),
+                           (-1,1),(0,1),(1,1)] if color == 'black' else [(0,-2), (-1,-1),(0,-1),(1,-1)]
 
     def getIcon(self): return self.icon
