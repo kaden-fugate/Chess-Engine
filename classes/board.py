d7a5b088f16b7fbfccc7761ca2368615a3148be6
kaden-fugate <kkadenfugate@gmail.com>
2023-12-22 21:45:54 -0800

    Castling + Checking if Move Kills King
    Made a function that will allow the user to make a move then it will switch to the other colors turn. Also implemented the functionalities for castling. Finally, also added the ability to check if a move will kill the king. This means either you move a piece and expose your king to an attack or your king is currently in check and your attempted move will not take it out of check. Final functionalities required to implement are en passant, checkmate, and stalemate. stalemate will include no valid moves for a color as well as an insufficient material stalemate.

--- classes/board.py
+++ classes/board.py
@@ -33,8 +33,7 @@ class Board:
         self.board[rank][1] = Knight(1, rank, color)
         self.board[rank][2] = Bishop(2, rank, color)
         self.board[rank][3] = Queen(3, rank, color)
-        if color == 'white': self.board[rank][4] = Queen(4, rank, color)
-        else: self.board[rank][4] = King(4, rank, color)
+        self.board[rank][4] = King(4, rank, color)
         self.board[rank][5] = Bishop(5, rank, color)
         self.board[rank][6] = Knight(6, rank, color)
         self.board[rank][7] = Rook(7, rank, color)
@@ -45,11 +44,19 @@ class Board:
         # set the pawns 
         for i in range(0,8): self.board[rank][i] = Pawn(i, rank, color)
 
+    def updatePieces(self):
+
+        for row in self.board:
+            for piece in row:
+
+                if piece:
+                    piece.setValidMoves(self.board)
+
     # function to print board
     def printBoard(self):
 
         # top of board
-        print("BOARD:\n_________________________________________________")
+        print("_________________________________________________")
 
         # rows in board
         for idx, row in enumerate(reversed(self.board)):
@@ -107,9 +114,31 @@ class Board:
                         for move in piece.getValidMoves():
                             opposite_color_moves.add(move)
 
-                if (king_x, king_y) in opposite_color_moves: return True
+                if (king_x, king_y) in opposite_color_moves:
+                    return True
 
         return False
+
+    def takeTurn(self):
+
+        turn = 'White' if self.current_turn == 'white' else 'Black'
+        print(f"\n{turn}'s Turn:")
+        self.printBoard()
+
+        while True:
+            try:
+                move = input("Please Input Your Move: ")
+
+                start = self.notationToPos(move[0] + move[1])
+                end = self.notationToPos(move[2] + move[3])
+
+                self.movePiece(start, end)
+                break
+            
+            except invalidMoveError as err:
+                print(err)
+
+        self.current_turn = 'white' if self.current_turn == 'black' else 'black'
 
     def movePiece(self, start_pos, end_pos):
 
@@ -134,48 +163,121 @@ class Board:
         elif end_pos not in start.getValidMoves(): 
             raise invalidMoveError(f"Invalid Move Error: Piece at {start_pos} can't Move to {end_pos}")
         
+        if self.isAttemptingToCastle(start_pos, end_pos):
+
+            self.board[start_y][start_x].canCastle(self.board, self.current_turn)
+
+            # short castle 
+            if end_x - start_x > 0:
+
+                # if the king can castle short
+                if self.board[start_y][start_x].getCastleShort():
+                    self.castle(True)
+                
+                # raise error if the king couldnt castle
+                else: raise invalidMoveError(f"Invalid Move Error: King at {start_pos} can't Castle Short")
+
+            # long castle
+            else:
+
+                if self.board[start_y][start_x].getCastleLong(): self.castle(False)
+
+                # raise error if the king couldnt castle
+                else: raise invalidMoveError(f"Invalid Move Error: King at {start_pos} can't Castle Long")
+
         # move the piece
         self.board[end_y][end_x] = start
         self.board[start_y][start_x] = None
+        self.updatePieces()
+
+        # if this move will put our king in check, the move is invalid. reverse the move and
+        # raise invalid move error
+        if self.isCheck(self.board, self.current_turn):
+            self.board[start_y][start_x] = start
+            self.board[end_y][end_x] = None
+            self.updatePieces()
+            raise invalidMoveError(f"Invalid Move Error: Moving Piece from {start_pos} to {end_pos} will Kill the King")
+
+        # update the pieces coordinates and set its "hasMoved" data member
+        self.board[end_y][end_x].setCoords((end_x, end_y))
+        self.board[end_y][end_x].setHasMoved()
+
+    def castle(self, is_short):
+
+        # get rank and file for easy indexing
+        rank = 0 if self.current_turn == 'white' else 7
+        rook_file = 5 if is_short else 3
+
+        # all of our pieces we might need
+        short_rook = self.board[rank][7]
+        long_rook = self.board[rank][0]
+
+        # if we're dealing with a short castle, we can set the new coords, update has moved, move
+        # the piece to its new place and delete it from the old position
+        if is_short:
+
+            short_rook.setCoords((rook_file,rank))
+            short_rook.setHasMoved()
+            self.board[rank][rook_file] = short_rook
+            self.board[rank][7] = None
+
+        # same rules with long castle as we have for short but we'll be dealing with the 
+        # 'long_rook' variable now instead of the 'short_rook' one
+        else:
+
+            long_rook.setCoords((rook_file,rank))
+            long_rook.setHasMoved()
+            self.board[rank][rook_file] = long_rook
+            self.board[rank][0] = None
 
     def isAttemptingToCastle(self, start_pos, end_pos):
+        
+        long_rook = (7,0) if self.current_turn == 'white' else (0,7)
+        short_rook = (0,0) if self.current_turn == 'white' else (7,7)
+
+        start_x, start_y = start_pos
+        end_x, end_y = end_pos
+
+        # if current piece is king
+        if self.board[start_y][start_x].getIcon() == '♔' or self.board[start_y][start_x].getIcon() == '♚':
+
+            # and end pos is two from start pos (x axis), then king is attempting to castle
+            if abs(start_x - end_x) == 2: return True
+        
+        # else, king not attempting case
+        return False
 
     def notationToPos(self, notation):
 
         pos_x, pos_y = -1, -1
 
+        if notation == 'OO':
+            return (6,0) if self.current_turn == 'white' else (6,7)
+        elif notation == 'OOO':
+            return (2,0) if self.current_turn == 'white' else (2,7)
+
         # convert rank to y pos
-        if notation[0] == 'A': pos_y = 0
-        elif notation[0] == 'B': pos_y = 1
-        elif notation[0] == 'C': pos_y = 2
-        elif notation[0] == 'D': pos_y = 3
-        elif notation[0] == 'E': pos_y = 4
-        elif notation[0] == 'F': pos_y = 5
-        elif notation[0] == 'G': pos_y = 6
-        elif notation[0] == 'H': pos_y = 7
+        if notation[0] == 'A' or notation[0] == 'a': pos_x = 0
+        elif notation[0] == 'B' or notation[0] == 'b': pos_x = 1
+        elif notation[0] == 'C' or notation[0] == 'c': pos_x = 2
+        elif notation[0] == 'D' or notation[0] == 'd': pos_x = 3
+        elif notation[0] == 'E' or notation[0] == 'e': pos_x = 4
+        elif notation[0] == 'F' or notation[0] == 'f': pos_x = 5
+        elif notation[0] == 'G' or notation[0] == 'g': pos_x = 6
+        elif notation[0] == 'H' or notation[0] == 'h': pos_x = 7
 
         # convert file to x pos
-        if notation[1] == '1': pos_x = 0
-        elif notation[1] == '2': pos_x = 1
-        elif notation[1] == '3': pos_x = 2
-        elif notation[1] == '4': pos_x = 3
-        elif notation[1] == '5': pos_x = 4
-        elif notation[1] == '6': pos_x = 5
-        elif notation[1] == '7': pos_x = 6
-        elif notation[1] == '8': pos_x = 7
+        if notation[1] == '1': pos_y = 0
+        elif notation[1] == '2': pos_y = 1
+        elif notation[1] == '3': pos_y = 2
+        elif notation[1] == '4': pos_y = 3
+        elif notation[1] == '5': pos_y = 4
+        elif notation[1] == '6': pos_y = 5
+        elif notation[1] == '7': pos_y = 6
+        elif notation[1] == '8': pos_y = 7
 
         return (pos_x, pos_y)
 
 chess = Board()
-chess.printBoard()
-for idx_row, row in enumerate(chess.board):
-    for idx_col, piece in enumerate(row):
-        pass
-        # if piece:
-            # print(f"- POSSIBLE MOVES FOR {piece.getIcon()} AT ({idx_col},{idx_row}): {piece.getValidMoves()}")
-
-# black_moves, white_moves = chess.getAllValidMoves()
-#print(f"- BLACK VALID MOVES: {black_moves}\n- WHITE VALID MOVES: {white_moves}")
-print(f"- BLACK IS IN CHECK: {chess.isCheck(chess.board, 'black')}")
-chess.movePiece((0,1),(0,2))
-chess.printBoard()
+for _ in range(0, 20):
+    chess.takeTurn()
--- classes/pieces.py
+++ classes/pieces.py
@@ -8,12 +8,17 @@ class Piece:
         self.valid_moves = []
         self.pos_x = pos_x
         self.pos_y = pos_y
-    
+
     def getColor(self):
         return self.color
     
     def getValidMoves(self):
         return self.valid_moves
+    
+    def getCoords(self): return (self.pos_x, self.pos_y)
+    
+    def getHasMoved(self):
+        pass
 
     def getIcon():
         pass
@@ -24,6 +29,9 @@ class Piece:
     def setCoords(self, coords):
         self.pos_x, self.pos_y = coords
 
+    def setHasMoved(self):
+        pass
+
 # King class
 class King(Piece):
 
@@ -32,17 +40,22 @@ class King(Piece):
         super().__init__(pos_x, pos_y, color)
         self.icon = '\u265A' if color == 'white' else '\u2654'
         self.directions = [(-1,1), (0,1), (1,1),
-                           (-1,0),        (1,0),
-                           (-1,-1),(0,-1),(1,-1)]
-        self.can_castle = True
+                   (-2,0), (-1,0),        (1,0), (2,0),
+                           (-1,-1),(0,-1),(1,-1) ]
+        self.can_castle_long = False
+        self.can_castle_short = False
+        self.hasMoved = False
 
     # kings icon
     def getIcon(self): return self.icon
-                
-    def isCheck(self):
-        return False
-
+    def getHasMoved(self): return self.hasMoved
+    def getCastleLong(self): return self.can_castle_long
+    def getCastleShort(self): return self.can_castle_short
+    
+    def setShortCastle(self, truth_val): self.can_castle_short = truth_val
+    def setLongCastle(self, truth_val): self.can_castle_long = truth_val
     def setValidMoves(self, board):
+        self.valid_moves = []
         
         for dx, dy in self.directions:
 
@@ -54,6 +67,53 @@ class King(Piece):
                 # check move does not put king in check
                 if not board_pos or (board_pos and board_pos.getColor() != self.color):
                     self.valid_moves.append((self.pos_x + dx, self.pos_y + dy))
+    
+    def setHasMoved(self):
+        self.hasMoved = True
+
+    def canCastle(self, board, turn):
+
+        # expected coords for all pieces
+        long_rook = (0,0) if turn == 'white' else (0,7)
+        short_rook = (7,0) if turn == 'white' else (7,7)
+        king = (4,0) if turn == 'white' else (4,7)
+
+        # icon for rook piece
+        rook_icon = '♖' if turn == 'black' else '♜'
+
+        # unpack all of the expected coords into their x,y counter-parts for easy indexing
+        king_x, king_y = king
+        short_x, short_y = short_rook
+        long_x, long_y = long_rook
+
+        # initially set can castle to true for each type of castle. they will be set to false if
+        # the king cannot castle that way.
+        self.can_castle_long = True
+        self.can_castle_short = True
+
+        # if king not in correct position, king cannot castle
+        if self.pos_x != king_x or self.pos_y != king_y or self.hasMoved: 
+            self.can_castle_long = False
+            self.can_castle_short = False
+
+        # if short rook not in correct position, king cannot castle
+        if not board[short_y][short_x] or (board[short_y][short_x].getIcon() != rook_icon or board[short_y][short_x].getHasMoved()): 
+            self.can_castle_short = False
+
+        # if king/rook is blocked, king cannot castle
+        for i in [5,6]: 
+            if board[king_y][i]: 
+                self.can_castle_short = False
+
+        # if long rook not in correct position, king cannot castle
+        if not board[long_y][long_x] or (board[long_y][long_x].getIcon() != rook_icon or board[long_y][long_x].getHasMoved()): 
+            self.can_castle_long = False
+
+        # if king/rook is blocked, king cannot castle
+        for i in [1,2,3]: 
+            if board[king_y][i]: self.can_castle_long = False
+    
+    
 
 class Queen(Piece):
 
@@ -72,6 +132,8 @@ class Queen(Piece):
     def getIcon(self): return self.icon
 
     def setValidMoves(self, board):
+        self.valid_moves = []
+
         for direction in self.directions:
 
             for dx, dy in direction:
@@ -81,9 +143,9 @@ class Queen(Piece):
 
                     board_pos = board[self.pos_y + dy][self.pos_x + dx]
                     
-                    # check move does not put king in check
                     if not board_pos or (board_pos and board_pos.getColor() != self.color):
                         self.valid_moves.append((self.pos_x + dx, self.pos_y + dy))
+                        if board_pos and board_pos.getColor() != self.color: break
                     else: break
 
 class Rook(Piece):
@@ -96,10 +158,15 @@ class Rook(Piece):
                             [(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)],
                             [(0,-1),(0,-2),(0,-3),(0,-4),(0,-5),(0,-6),(0,-7)] ]
         self.can_castle = True
+        self.hasMoved = False
 
     def getIcon(self): return self.icon
 
+    def getHasMoved(self): return self.hasMoved
+
     def setValidMoves(self, board):
+        self.valid_moves = []
+
         for direction in self.directions:
 
             for dx, dy in direction:
@@ -112,7 +179,11 @@ class Rook(Piece):
                     # check move does not put king in check
                     if not board_pos or (board_pos and board_pos.getColor() != self.color):
                         self.valid_moves.append((self.pos_x + dx, self.pos_y + dy))
+                        if board_pos and board_pos.getColor() != self.color: break
                     else: break
+    
+    def setHasMoved(self):
+        self.hasMoved = True
 
 class Bishop(Piece):
 
@@ -127,6 +198,8 @@ class Bishop(Piece):
     def getIcon(self): return self.icon
 
     def setValidMoves(self, board):
+        self.valid_moves = []
+
         for direction in self.directions:
 
             for dx, dy in direction:
@@ -139,6 +212,7 @@ class Bishop(Piece):
                     # check move does not put king in check
                     if not board_pos or (board_pos and board_pos.getColor() != self.color):
                         self.valid_moves.append((self.pos_x + dx, self.pos_y + dy))
+                        if board_pos and board_pos.getColor() != self.color: break
                     else: break
 
 class Knight(Piece):
@@ -154,6 +228,7 @@ class Knight(Piece):
     def getIcon(self): return self.icon
 
     def setValidMoves(self, board):
+        self.valid_moves = []
         
         for dx, dy in self.directions:
 
@@ -177,6 +252,7 @@ class Pawn(Piece):
     def getIcon(self): return self.icon
 
     def setValidMoves(self, board):
+        self.valid_moves = []
 
         for idx, direction in enumerate(self.directions):
 
@@ -191,7 +267,6 @@ class Pawn(Piece):
                 # if idx 0 (2 square jump), and initial jump variable is true 
                 # and the two square required in jump are not blocked (board_pos 
                 # and prev_pos), 2 square jump is valid move
-
                 if idx == 0 and self.initial_jump and (not board_pos and not prev_pos):
                     self.valid_moves.append( (new_x, new_y) )
 
