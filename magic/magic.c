5eae2fa48a122b428e9c292d83a57a00b003b5dc
kaden-ator <kkadenfugate@gmail.com>
2024-08-28 21:46:47 -0700

    implemented attack generation for rooks and bishops. addtionally, added a print_bitboard function.

--- magic/magic.c
+++ magic/magic.c
@@ -24,6 +24,8 @@
 // |----|----|----|----|----|----|----|----|
 // |  + |    |    |    |    |    |    |    |
 // |----|----|----|----|----|----|----|----|
+// |  + |    |    |    |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
 // |  * |  + |  + |  + |  + |  + |  + |    |
 // |----|----|----|----|----|----|----|----|
 // |  + |    |    |    |    |    |    |    |
@@ -54,6 +56,36 @@ uint64_t r_mask(uint8_t pos){
 
 }
 
+//
+// this function will be the same as r_mask above but for a bishop instead.
+// for this, pieces will move diagonally instead of vertically and horizontally
+// (as you probably know by now). 
+// 
+// just as before, this bitmap can be stored in a hash table for quick lookup
+// when we use it later. 
+//
+// example of a mask gen given a square (square 18):
+//
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |    |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |    |    |    |  + |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |    |    |  + |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |    |  + |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |  + |    |  + |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |  * |    |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |  + |    |  + |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |    |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+//
+// * = position, + = possible blockers
+//
 uint64_t b_mask(uint8_t pos){
 
     uint64_t mask = 0x0;
@@ -79,9 +111,193 @@ uint64_t b_mask(uint8_t pos){
 
 }
 
+//
+// this function will take a rooks position as well as its blockers. with this
+// we will create a bitmap representing all possible moves for the rook given 
+// its position and the position of its blockers.
+//
+// this is an expensive function so the idea of this is that we're going to
+// calculate this attack ahead of time and just lookup the precalculated 
+// moveset whenever we want at runtime (using methods implemented below).
+//
+// example of a bitboard (pos: 27, blockers: 0x8000804080800)
+//
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |    |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |  + |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |    |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |+ 1 |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |+ 1 | 1  |  * |  1 |  1 |  1 |  1 |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |+ 1 |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |  + |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |    |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+//
+// * = position, + = blockers, 1 = square included in bitboard
+//
+// (just as a note, a square containing '+ 1' is a square where a blocker is 
+// present and the square is also included in the bitboard)
+//
+uint64_t r_attacks(uint8_t pos, uint64_t blockers){
+
+    int8_t x = pos % 8, y = pos / 8, dx, dy;
+    uint64_t result = 0x0, observed = 0x0;
+
+    // calculate eastern moves
+    for (dx = x + 1; dx < 8; dx++){
+
+        observed = (1ULL << (y * 8 + dx)); 
+
+        result |= observed;
+        if (blockers & observed) break;
+
+    }
+
+    // calculate western moves
+    for (dx = x - 1; dx >= 0; dx--){
+
+        observed = (1ULL << (y * 8 + dx)); 
+        
+        result |= observed;
+        if (blockers & observed) break;
+
+    }
+
+    // calculate northern moves
+    for (dy = y + 1; dy < 8; dy++){
+
+        observed = (1ULL << (dy * 8 + x)); 
+        
+        result |= observed;
+        if (blockers & observed) break;
+
+    }
+
+    // calculate southern moves
+    for (dy = y - 1; dy >= 0; dy--){
+
+        observed = (1ULL << (dy * 8 + x)); 
+        
+        result |= observed;
+        if (blockers & observed) break;
+
+    }
+
+    return result;
+
+} 
+
+//
+// once again, this function is the same as the above r_attacks but for bishops
+// instead. a bitmap with all valid moves from position with given blockers 
+// will be returned.
+//
+// example of a bitboard (pos: 27, blockers: 0x201000002200)
+//
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |    |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |  1 |    |    |    |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |  1 |    |    |    |  + |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |  1 |    |+ 1 |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |  * |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |  1 |    |  1 |    |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |+ 1 |    |    |    |+ 1 |    |    |
+// |----|----|----|----|----|----|----|----|
+// |    |    |    |    |    |    |    |    |
+// |----|----|----|----|----|----|----|----|
+//
+// * = position, + = blockers, 1 = square included in bitboard
+//
+// (just as a note, a square containing '+ 1' is a square where a blocker is 
+// present and the square is also included in the bitboard)
+//
+uint64_t b_attacks(uint8_t pos, uint64_t blockers){
+
+    int8_t x = pos % 8, y = pos / 8, dx, dy;
+    uint64_t result = 0x0, observed = 0x0;
+
+    // calculate north-eastern moves
+    for (dx = x + 1, dy = y + 1; dx < 8 && dy < 8; dx++, dy++){
+
+        observed = (1ULL << (dy * 8 + dx)); 
+
+        result |= observed;
+        if (blockers & observed) break;
+
+    }
+
+    // calculate north-western moves
+    for (dx = x - 1, dy = y + 1; dx >= 0 && dy < 8; dx--, dy++){
+
+        observed = (1ULL << (dy * 8 + dx)); 
+
+        result |= observed;
+        if (blockers & observed) break;
+
+    }
+
+    // calculate south-eastern moves
+    for (dx = x + 1, dy = y - 1; dx < 8 && dy >= 0; dx++, dy--){
+
+        observed = (1ULL << (dy * 8 + dx)); 
+
+        result |= observed;
+        if (blockers & observed) break;
+
+    }
+
+    // calculate south-western moves
+    for (dx = x - 1, dy = y - 1; dx >= 0 && dy >= 0; dx--, dy--){
+
+        observed = (1ULL << (dy * 8 + dx)); 
+
+        result |= observed;
+        if (blockers & observed) break;
+
+    }
+
+    return result;
+
+} 
+
+// 
+// this function just prints a given bitboard. just makes debugging easier
+//
+void print_bitboard(uint64_t board){
+
+    for (int8_t i = 7; i >= 0; i--){
+
+        for (int8_t j = 0; j < 8; j++){
+
+            if (i * 8 + j == 27) {printf("   *   "); continue;}
+            printf("   %d   ", (board & (1ULL << (i * 8 + j))) ? 1 : 0);
+
+        }
+
+        printf("\n\n");
+
+    }
+
+    printf("\n");
+
+}
+
 int main(){
 
-     
+    
 
     return 0;
 
